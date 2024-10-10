cd268c44a7e965035bcb52083d47631e28f40899
Andrei Likhoded <liberty102and8131186@gmail.com>
2024-10-10 14:07:49 +0600

    third player is added

--- src/game/Game.java
+++ src/game/Game.java
@@ -8,8 +8,8 @@ public class Game {
     public static void startGame() {
         int firstPlayerScore = 0;
         int secondPlayerScore = 0;
-        Scanner scanner1 = new Scanner(System.in);
-        Scanner scanner2 = new Scanner(System.in);
+        int thirdPlayerScore = 0;
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("ИГРА ОРЁЛ И РЕШКА");
         System.out.println("------------------");
@@ -21,7 +21,7 @@ public class Game {
             while (firstPlayerMove < 0 || firstPlayerMove > 1) {
                 try {
                     System.out.println("[Игрок 1] ваша ставка (1 - орёл, 0 - решка):");
-                    firstPlayerMove = Integer.parseInt(scanner1.nextLine());
+                    firstPlayerMove = Integer.parseInt(scanner.nextLine());
                     if (firstPlayerMove < 0 || firstPlayerMove > 1) {
                         System.out.println("Ошибка ввода");
                     }
@@ -30,15 +30,12 @@ public class Game {
                 }
             }
 
-            if (firstPlayerMove == coinRandom) {
-                firstPlayerScore++;
-            }
 
             int secondPlayerMove = -1;
             while (secondPlayerMove < 0 || secondPlayerMove > 1) {
                 try {
                     System.out.println("[Игрок 2] ваша ставка (1 - орёл, 0 - решка):");
-                    secondPlayerMove = Integer.parseInt(scanner2.nextLine());
+                    secondPlayerMove = Integer.parseInt(scanner.nextLine());
                     if (secondPlayerMove < 0 || secondPlayerMove > 1) {
                         System.out.println("Ошибка ввода");
                     }
@@ -47,15 +44,27 @@ public class Game {
                 }
             }
 
-            if (secondPlayerMove == coinRandom) {
-                secondPlayerScore++;
+
+
+            int thirdPlayerMove = -1;
+            while(thirdPlayerMove < 0 || thirdPlayerMove > 1){
+                try {
+                    System.out.println("[Игрок 3] ваша ставка (1 - орёл, 0 - решка):");
+                    thirdPlayerMove = Integer.parseInt(scanner.nextLine());
+                    if(thirdPlayerMove < 0 || thirdPlayerMove > 1){
+                        System.out.println("Ошибка ввода");
+                    }
+                }catch (NumberFormatException e){
+                    System.out.println("Ошибка ввода");
+                }
             }
-            printResult(coinRandom, firstPlayerMove, secondPlayerMove);
+
+            printResult(coinRandom, firstPlayerMove, secondPlayerMove, thirdPlayerMove);
             break;
         }
     }
 
-    public static void printResult(int coinRandom, int firstPlayerMove, int secondPlayerMove) {
+    public static void printResult(int coinRandom, int firstPlayerMove, int secondPlayerMove, int thirdPlayerMove) {
         System.out.println("Подкинули монетку, выпала сторона: " + (coinRandom == 1 ? "орёл" : "решка"));
         System.out.println();
 
@@ -66,5 +75,8 @@ public class Game {
 
         System.out.printf("%-6s  %-6s  %-6s\n", "Игрок 2", secondPlayerMove == 1 ? "[ставка орёл]  :" : "[ставка решка] :",
                 (secondPlayerMove == coinRandom) ? "выиграл" : "проиграл");
+
+        System.out.printf("%-6s  %-6s  %-6s\n", "Игрок 3", thirdPlayerMove == 1 ? "[ставка орёл]  :" : "[ставка решка] :",
+                (thirdPlayerMove == coinRandom) ? "выиграл" : "проиграл");
     }
 }
